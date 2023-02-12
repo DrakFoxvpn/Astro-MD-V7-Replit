4e73b28ca1e23060294f178566e03b64c9e9cdd0
Vihanga Ashinshana <96816258+vihangayt0@users.noreply.github.com>
2023-02-12 13:26:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,33 @@
-const { Sequelize } = require('sequelize');
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+const { Sequelize } = require(`sequelize`);
+const fs = require(`fs`);
+if (fs.existsSync(`config.env`)) require(`dotenv`).config({ path: `./config.env` });
 
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
+
 module.exports = {
-ALIVE_MSG: process.env.ALIVE_MSG === undefined ? 'default' : process.env.ALIVE_MSG,
-LANG: process.env.LANG === undefined ? 'EN' : process.env.LANG,
-ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.imgur.com/v8iuP8v.jpg' : process.env.ALIVE_LOGO,
-FOOTER: process.env.FOOTER === undefined ? 'ᴀꜱᴛʀᴏ-ᴍᴅ ᴘᴜʙʟɪᴄ ꜱᴛᴀʙʟᴇ' : process.env.FOOTER,
-CAPTION: process.env.CAPTION === undefined ? '*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ᴀꜱᴛʀᴏ-ᴍᴅ*' : process.env.CAPTION,
-ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
-INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
-HOST: process.env.HOST === undefined ? '-' : process.env.HOST,
-SESSION_ID: process.env.SESSION_ID === undefined ? 'ZjlyUHg_astro_3TXY=' : process.env.SESSION_ID
+
+
+ALIVE_MSG: `default` ,
+
+LANG: `EN` ,
+
+ALIVE_LOGO: `https://i.imgur.com/v8iuP8v.jpg` ,
+
+FOOTER: `ᴀꜱᴛʀᴏ-ᴍᴅ ᴘᴜʙʟɪᴄ ꜱᴛᴀʙʟᴇ` ,
+
+CAPTION: `*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ᴀꜱᴛʀᴏ-ᴍᴅ*` ,
+
+ONLY_GROUP: `false`,
+
+INBOX_BL_MSG: `*Inbox not allowed` ,
+
+HOST: `-` ,
+
+SESSION_ID: `ASTRO=pihCDbyR#cQ9eoJrRzWnWV5-bclrKwVBw06NhPWrsz2vPMcPnJ0s` 
+
+
 };
